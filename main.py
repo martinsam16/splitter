a3f9c5dd9d79cb78b6c120ba09815f19fce65a96
Martín Alexis Samán Arata <martin.saman@vallegrande.edu.pe>
2020-10-26 23:49:37 -0500

    add ui
    pyqt5 and requirements.txt

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
+import os
+
 from splitter.AudioSplitter import AudioSplitter
 from splitter.ImageSplitter import ImageSplitter
 from splitter.VideoSplitter import VideoSplitter
 
-path_video = './video.mp4'
+from ui import *
 
-# split video range (minutes, seconds)
-start_video = (0, 0)
-end_video = (6, 12)
 
-# split per n minutes
-minutes_per_split_audio = 2
-seconds_per_split_video = 30
+class MainWindow(QtWidgets.QDialog, Ui_Dialog):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+        self.pushButtonOk.clicked.connect(self.process)
 
-# percent of similarity between images for delete
-min_similarity_images = 0.90
+    def process(self):
+        # paths save
+        folder_save = self.inptOutputFolder.toPlainText()
+        if not os.path.exists(folder_save):
+            os.mkdir(folder_save)
+        path_audio_save = os.path.join(folder_save, 'audio.wav')
+        folder_audio_splitted = os.path.join(folder_save, 'audio_splitted')
+        folder_images = os.path.join(folder_save, 'images')
+        path_text_save = os.path.join(folder_save, 'text.txt')
 
-# paths save
-path_audio_save = 'audio.wav'
-folder_audio_splitted = './audio_splitted/'
-folder_images = './images/'
-path_text_save = './text.txt'
+        # split video range (minutes, seconds)
+        path_video = self.inptPathVideo.toPlainText()
+        start_video = (self.spinMinStart.value(), self.spinSecStart.value())
+        end_video = (self.spinMinEnd.value(), self.spinSecEnd.value())
 
-video_splitter = VideoSplitter(path_video=path_video, start=start_video, end=end_video)
-video_splitter.split_images(path_folder_save=folder_images, seconds_per_split=seconds_per_split_video)
-video_splitter.extract_audio(path_file_save=path_audio_save)
+        # split per n minutes
+        minutes_per_split_audio = 2
+        seconds_per_split_video = self.spinSecSplit.value()
 
-audio_splitter = AudioSplitter(path_audio=path_audio_save, path_folder_split=folder_audio_splitted)
-audio_splitter.multiple_split(min_per_split=minutes_per_split_audio)
+        # percent of similarity between images for delete
+        min_similarity_images = self.doubleSpinSimilarity.value()
 
-image_splitter = ImageSplitter()
-image_splitter.delete_image_similarity(path_folder_images=folder_images, min_similarity=min_similarity_images)
+        video_splitter = VideoSplitter(path_video=path_video, start=start_video, end=end_video)
+        video_splitter.split_images(path_folder_save=folder_images, seconds_per_split=seconds_per_split_video)
 
-AudioSplitter.extract_text_from_folder(path_folder=folder_audio_splitted, path_txt=path_text_save)
+        image_splitter = ImageSplitter()
+        image_splitter.delete_image_similarity(path_folder_images=folder_images, min_similarity=min_similarity_images)
+
+        # Extract speech
+        if self.checkBoxSpeechRecognition.isChecked():
+            video_splitter.extract_audio(path_file_save=path_audio_save)
+            audio_splitter = AudioSplitter(path_audio=path_audio_save, path_folder_split=folder_audio_splitted)
+            audio_splitter.multiple_split(min_per_split=minutes_per_split_audio)
+            AudioSplitter.extract_text_from_folder(path_folder=folder_audio_splitted, path_txt=path_text_save)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
